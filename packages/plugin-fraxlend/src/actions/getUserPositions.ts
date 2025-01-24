20e3d2b801f54a349313e29ebae57f662ea1c629
Srujan Gurram <srujangs8@gmail.com>
2025-01-24 14:51:45 +0530

    Refactor user positions query and data mapping logic

--- packages/plugin-fraxlend/src/actions/getUserPositions.ts
+++ packages/plugin-fraxlend/src/actions/getUserPositions.ts
@@ -2,20 +2,31 @@ import { graphql } from "gql.tada";
 import { client } from "../lib/graphql";
 
 const USER_POSITIONS_QUERY = graphql(`
-  query GetUserPositions($userAddress: String!) {
-    positions(where: {user: $userAddress}) {
-      pair {
-        symbol
-        asset {
+  query fraxlendUsers($user: User_filter) {
+    users(first: 1000, where: $user) {
+      id
+      positions {
+        borrowedAssetShare
+        lentAssetShare
+        depositedCollateralAmount
+        pair {
           symbol
+          asset {
+            symbol
+            decimals
+          }
+          collateral {
+            symbol
+            decimals
+          }
         }
-      }
-      lentAssetShare
-      depositedCollateralAmount
-      dailyHistory(first: 1, orderBy: timestamp, orderDirection: desc) {
-        lentAssetValue
-        lendProfitTaken
-        timestamp
+        dailyHistory(first: 1, orderBy: timestamp, orderDirection: desc) {
+          lentAssetValue
+          lendProfitTaken
+          borrowedAssetValue
+          depositedCollateralValue
+          timestamp
+        }
       }
     }
   }
@@ -24,16 +35,24 @@ const USER_POSITIONS_QUERY = graphql(`
 export async function getUserPositions(userAddress: string) {
 	try {
 		const data = await client.request(USER_POSITIONS_QUERY, {
-			userAddress,
+			user: {
+				id: userAddress.toLowerCase(),
+			},
 		});
+
 		return {
 			success: true,
-			data: data.positions.map((position) => ({
+			data: data.users[0]?.positions.map((position) => ({
 				symbol: position.pair.symbol,
 				assetSymbol: position.pair.asset.symbol,
+				collateralSymbol: position.pair.collateral.symbol,
 				lentAmount: position.lentAssetShare,
+				borrowedAmount: position.borrowedAssetShare,
 				collateralAmount: position.depositedCollateralAmount,
 				value: position.dailyHistory[0]?.lentAssetValue || "0",
+				borrowValue: position.dailyHistory[0]?.borrowedAssetValue || "0",
+				collateralValue:
+					position.dailyHistory[0]?.depositedCollateralValue || "0",
 				profit: position.dailyHistory[0]?.lendProfitTaken || "0",
 			})),
 		};
