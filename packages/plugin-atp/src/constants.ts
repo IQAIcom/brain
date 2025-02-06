bab90f3f48112a2d48a16a3214119254130ae48c
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-02-06 16:57:14 +0530

    create

---
+++ packages/plugin-atp/src/constants.ts
@@ -0,0 +1,5 @@
+export const API_URLS= {
+  BASE: "https://app.iqai.com/api",
+  AGENTS_STATS: "https://app.iqai.com/api/agents/stats",
+  HOLDINGS: "https://app.iqai.com/api/holdings",
+} as const;
